02f9b3fd26f366494c9418e26800aad5484e5540
gpe-soft <peterse73@gmail.com>
2016-09-29 23:42:10 +0200

    example from angular.io

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,4 @@
-//main entry point
-import {bootstrap} from '@angular/platform-browser-dynamic';
-import {App} from './app';
-
-bootstrap(App, [])
-    .catch(err => console.error(err));
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
